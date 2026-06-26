32736c07ce464c715373fc3f43490f65bd7042b2
Rabbau <oprovozenko@gmail.com>
2026-06-26 17:51:35 +0300

    fix: 0.2.4.7

--- api/[...path].js
+++ api/[...path].js
@@ -18,7 +18,7 @@ const UPSTREAM = {
   analytics: 'https://api.deadlock-api.com',
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
