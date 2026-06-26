7c2afd75286ed1b61225e44836c48cab44792b68
Rabbau <oprovozenko@gmail.com>
2026-06-26 18:06:10 +0300

    fix: vercel depend

--- api/[...path].js
+++ api/[...path].js
@@ -20,6 +20,15 @@ const UPSTREAM = {
 
 export default async function handler(req, res) {
   // CORS
+    console.log("URL:", req.url);
+    console.log("QUERY:", req.query);
+
+    res.status(200).json({
+        url: req.url,
+        query: req.query
+      });
+    
+
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
